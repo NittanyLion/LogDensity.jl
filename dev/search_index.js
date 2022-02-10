405b25b99a3d3e710f8adad10524f21218239eec
Documenter.jl <documenter@juliadocs.github.io>
2022-02-10 00:07:46 +0000

    build based on ee2a01e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LogDensity","category":"page"},{"location":"#LogDensity","page":"Home","title":"LogDensity","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LogDensity.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LogDensity]","category":"page"}]
+}
