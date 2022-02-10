287e2cdc47019b24a2525d1094b70a7700d66093
Joris Pinkse <pinkse@gmail.com>
2022-02-09 20:56:11 -0500

    final attempt

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,10 @@
 using LogDensity
 using Documenter
 
-# DocMeta.setdocmeta!(LogDensity, :DocTestSetup, :(using LogDensity); recursive=true)
-
-
-# format = Documenter.HTML(edit_link = "master",
-#                          prettyurls = get(ENV, "CI", nothing) == "true",
-#                          assets = String[],
-# )
 makedocs(;
     modules=[LogDensity],
     authors="Joris Pinkse <joris@psu.edu> and Karl Schurter",
-    # repo="https://github.com/NittanyLion/LogDensity.jl/blob/{commit}{path}#{line}",
     sitename="LogDensity.jl",
-    # format=Documenter.HTML(;
-    #     prettyurls=get(ENV, "CI", "false") == "true",
-    #     canonical="https://NittanyLion.github.io/LogDensity.jl",
-    #     assets=String[],
-    # ),
     pages=[
         "Home" => "index.md",
     ],
@@ -26,5 +13,4 @@ makedocs(;
 deploydocs(;
     repo = "github.com/NittanyLion/LogDensity.jl",
     target = "build"
-    # devbranch="main",
 )
